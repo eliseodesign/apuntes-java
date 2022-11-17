2590d6a2e324ff6430db0fce9b4de738b6df8478
Eliseo Arevalo <2480931@clases.edu.sv>
2022-11-16 19:40:22 -0600

    Bucles

--- soluciones/Soluciones_2_2_Cuestionario.java
+++ soluciones/Soluciones_2_2_Cuestionario.java
@@ -13,7 +13,7 @@ public class Soluciones_2_2_Cuestionario {
         int uno = scan.nextInt();
 
         if (uno == 1) {
-            puntaje += 1;
+            ++puntaje;
             System.out.println("correcto");
         } else {
             System.out.println("incorrecto");
@@ -23,14 +23,21 @@ public class Soluciones_2_2_Cuestionario {
         System.out.println("1: EDI      2: IDE      3: Editor");
         int dos = scan.nextInt();
         if (dos == 2) {
-            puntaje += 1;
+            ++puntaje;
         }
 
         System.out.println("Tipo de variable para almacenar decimales de gran tamaño");
         System.out.println("1: float      2: decimal      3: double");
         int tres = scan.nextInt();
         if (tres == 3) {
-            puntaje += 1;
+            ++puntaje;
+        }
+
+        System.out.println("En cada programa Java debe haber");
+        System.out.println("1: metodo main      2: dos variables      3: una libreria");
+        int cuatro = scan.nextInt();
+        if (cuatro == 1) {
+            ++puntaje;
         }
 
         System.out.println("---FIN---");
